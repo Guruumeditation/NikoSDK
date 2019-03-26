b04dcef60a21bf9c2d20de58a493d971178ab3b6
Olivier Matis <Guruumeditation@users.noreply.github.com>
2019-03-26 15:47:31 +0100

    Added pipelines

---
+++ Net.ArcanaStudio.NikoSDK.Shared/Interfaces/ISystemInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Net.ArcanaStudio.NikoSDK.Interfaces
+{
+    public interface ISystemInfo : IBaseResponse
+    {
+        string SwVersion { get; }
+        string Api { get; }
+        DateTime? Date { get; }
+        string Language { get; }
+        string Currency { get; }
+        string Units { get; }
+        string Dst { get; }
+        string Tz { get; }
+        DateTime? LastEnergyErase { get; }
+        DateTime? LastConfig { get; }
+    }
+}
